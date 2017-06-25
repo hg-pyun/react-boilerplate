4bf23c65b5888da43e7c72101e7eb9023a4ee988
haegul <phg2491@gmail.com>
2017-06-25 19:33:45 +0900

    add webpack-dev-server

---
+++ README.md
@@ -0,0 +1,18 @@
+# react-boilerplate
+This is simplest react boilerplate for [plate-cli](https://github.com/haegul/plate-cli).
+
+## Installation
+```
+$ npm install plate -g && plate react 
+```
+
+## Stack
+- Webpack
+-
+
+## Default Commands
+```
+$ npm run init
+$ npm run dev
+$ npm run build
+```
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const { resolve } = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry : ['./src/index.js'],
+    entry : {
+        "bundle" : "./src/index.js",
+    },
     output : {
-        filename : 'client.bundle.js',      // output filename
+        filename: "[name].js",              // output filename
         path : resolve(__dirname, 'build'), // output path
-        publicPath : '/'
     },
 
     module : {
@@ -22,6 +23,24 @@ module.exports = {
                 }
             }
         ],
+    },
+
+    plugins:[
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    devServer: {
+        hot : true,
+        inline: true,
+        host: '0.0.0.0',
+        disableHostCheck: true,
+        port: 3000,
+        open : true,
+        contentBase : "public",
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        }
     },
 
     devtool : 'inline-source-map'
